5747700c7924bfbc6b4bf9e0be5fd73ea2ccbbda
Ray Maldonado <vivachihuahua2004@gmail.com>
2023-05-15 16:53:25 -0500

    Chapter 4 - Final commit

--- Chapter_4_Lists_And_Loops/Expenses.py
+++ Chapter_4_Lists_And_Loops/Expenses.py
@@ -1,6 +1,13 @@
 
-expenses = [100, 200, 39.99, 49, 78.9, 809]
+expenses = []
+
+num_expenses = int(input("How many expenses your have\n"))
+
+for i in range(num_expenses):
+    expenses.append(float(input("Enter an expense\n")))
+
 total = sum(expenses)  # cool function from python
+print('You spent ', total)
 
 for expense in expenses:
     print(expense)
