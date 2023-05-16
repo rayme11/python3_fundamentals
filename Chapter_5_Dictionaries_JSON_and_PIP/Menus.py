ddf8619b4c8450e517419cca283c464cc0540ecd
Ray Maldonado <vivachihuahua2004@gmail.com>
2023-05-16 16:48:20 -0500

    Chapter 5 - Final commit

---
+++ Chapter_5_Dictionaries_JSON_and_PIP/Menus.py
@@ -0,0 +1,6 @@
+menus = {'Breakfast': ['eggs', 'bagel', 'orange juice'],
+         'Lunch': ['eggs', 'coffee', 'chicken'],
+         'Dinner': ['tea', 'bagel', 'turnkey']}
+
+for name, menu in menus.items():
+    print(name, ':', menu)
